d7fc2453e22e2f7add373628df523179343f3d13
BeyondCC <cclovedota@gmail.com>
2014-12-18 00:25:46 +0800

    BuyAndSellStock

--- BuyAndSellStock.cpp
+++ BuyAndSellStock.cpp
@@ -1,20 +1,38 @@
 #include<iostream>
+#include<vector>
+#include<cmath>
 
 using namespace std;
 
 int maxProfit(vector<int> &prices) {
-    int size = prices.size();
-    int *dp = new int[size];
 
-    for(int i = 0; i < size; i++) {
-        dp[i] = 0;
+    if(prices.size() == 0)
+        return 0;
+
+    int local = 0;
+    int global = 0;
+
+    for(int i = 1; i < prices.size(); i++) {
+        local = max(local + prices[i + 1] - prices[i], 0);
+        global = max(global, local);
     }
 
-    for(int i = 1; i < size; i++) {
-        dp[i] 
-    }
+    return global;
+}
 
-    return dp[size - 1];
+ int maxProfit1(vector<int> &prices) {
+       
+    if(prices.size() == 0)
+        return 0;
+        
+    int profit = 0;
+    int minNum = prices[0];
+    
+    for(int i = 1; i < prices.size(); i++) {
+        profit = max(profit, prices[i] - minNum); // to calculate the current max profit(p[i] - min)
+        minNum = min(minNum, prices[i]);     //record the minest
+    }
+    return profit;
 }
 
 

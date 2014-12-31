e68bb10c377a5298e375de0e949a1ec951c9d82d
七槐 <qihuai.cc@alibaba-inc.com>
2014-12-31 15:19:10 +0800

    InterLeaveString

--- InterLeaveString.cpp
+++ InterLeaveString.cpp
@@ -58,12 +58,37 @@ bool isInterleave(string s1, string s2, string s3) {
     return dp[s1.size()][s2.size()];   
 }
 
-int main(int argc, char *argv[]) {
-    string s1 = "aabcc";
-    string s2 = "dbbca";
-    string s3 = "aadbbcbcac";
+bool isInterleaveTune(string s1, string s2, string s3) {
+    if(s1.size() + s2.size() != s3.size())
+        return false;
 
-    cout<<"result is "<<isInterleave(s1, s2, s3);
+    string maxStr = s1.size() > s2.size() ? s1 : s2;
+    string minStr = s1.size() > s2.size() ? s2 : s1;
+
+    bool *dp = new bool[minStr.size() + 1];
+    dp[0] = true;
+
+    //the min str to init the dp, it means whether only use it can compose s3
+    for(int i = 0; i < minStr.size(); i++)
+        dp[i + 1] = dp[i] && minStr[i] == s3[i];
+
+    for(int i = 0; i < maxStr.size(); i++) {
+        dp[0] = dp[0] && maxStr[i] == s3[i]; // dp[0] stand for whether only use s1 can composite for s3
+        for(int j = 0; j < minStr.size(); j++) {
+            dp[j + 1] = (dp[j + 1] && maxStr[i] == s3[i + j + 1]) || (dp[j] &&minStr[j] == s3[i + j + 1]);
+        }
+    }
+    return dp[minStr.size()];
+}
+
+
+int main(int argc, char *argv[]) {
+    string s1 = "bcccb";
+    string s2 = "bccbbaba";
+    string s3 = "bacccccbbbbba";
+
+    cout<<"result is "<<isInterleave(s1, s2, s3)<<endl;
+    cout<<"result is "<<isInterleaveTune(s1, s2, s3)<<endl;
     return 0;
 }
 

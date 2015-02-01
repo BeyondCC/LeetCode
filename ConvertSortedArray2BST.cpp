45acac3cd2a84a33e6e1e67f7caac5ee5f4caa31
七槐 <qihuai.cc@alibaba-inc.com>
2015-02-01 21:48:13 +0800

    ConvertSortedArray2BST

---
+++ ConvertSortedArray2BST.cpp
@@ -0,0 +1,137 @@
+// Source : https://oj.leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+// Author : Cheng Chen
+// Date   : 2015-02-01
+
+/********************************************************************************** 
+  *  Given an array where elements are sorted in ascending order, convert it to a height balanced BST.
+  *               
+  **********************************************************************************/
+
+#include<iostream>
+#include<vector>
+#include<stack>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+vector<int> preorder(TreeNode *root);
+vector<int> preorder(TreeNode * root)
+{
+    vector<int> v;
+    vector<TreeNode*> stack;
+
+    if(root)
+        stack.push_back(root);
+
+    while(stack.size() > 0)
+    {
+        TreeNode *node = stack.back();
+        v.push_back(node->val);
+        stack.pop_back();
+
+        if(node->right)
+            stack.push_back(node->right);
+
+        if(node->left)
+            stack.push_back(node->left);
+    }
+    return v;
+}
+
+
+TreeNode* createTree(int a[], int n)
+{
+    if (n<=0) return NULL;
+
+        TreeNode **tree = new TreeNode*[n];
+
+        for(int i=0; i<n; i++) {
+            if (a[i]==0 ){
+                tree[i] = NULL;
+                continue;
+            }
+            tree[i] = new TreeNode(a[i]);
+        }
+                    
+		int pos=1;
+                        
+		for(int i=0; i<n && pos<n; i++) {
+                
+            if (tree[i]){
+		    tree[i]->left = tree[pos++];
+            
+            if (pos<n){
+                tree[i]->right = tree[pos++];                                                                            
+            }
+             
+            }
+        }
+        return tree[0];
+}
+
+void printTree_pre_order(TreeNode *root)
+{
+    if (root == NULL){
+        //cout << "# ";
+        return ;
+    }
+        
+    cout << root->val << " ";
+    printTree_pre_order(root->left);
+    printTree_pre_order(root->right);
+}
+
+void printArray(vector<int> v)
+{
+    for(int i=0; i<v.size(); i++){
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+
+TreeNode *helper(vector<int> &num, int start, int end)
+{
+    if (start > end)
+    {
+        return NULL;
+    }
+
+    int middle = (start + end) / 2;
+    TreeNode *current = new TreeNode(num[middle]);
+
+    current->left = helper(num, start, middle - 1);
+    current->right = helper(num, middle + 1, end);
+
+    return current;
+}
+
+TreeNode *sortedArrayToBST(vector<int> &num) {
+    return  helper(num, 0, num.size() - 1);
+}
+
+int main()
+{
+    vector<int> v;
+
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+    v.push_back(6);
+    v.push_back(7);
+    v.push_back(8);
+
+    sortedArrayToBST(v);
+
+    cout<<endl;
+
+
+    return 0;
+}

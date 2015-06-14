3fd34abbe042bf18e8241a452d45471b62bdbc28
BeyondCC <cclovedota@gmail.com>
2015-06-15 00:16:38 +0800

    Rectangle Area

--- RectangleArea.cpp
+++ RectangleArea.cpp
@@ -21,40 +21,69 @@
 
 using namespace std;
 
-int inArea(int point, int min, int max, int candidate, int flag)
+//flag 1 for upper 
+//compute the lap margin
+int inArea(int left, int right, int min, int max, int flag)
 {
-    if (min <= point && point <= max && candidate >= max) {
-        return point;
+    if (min <= left && right <= max) {
+        return  flag == 0 ? left : right;
     }
-    
-    //ont rectange wrap another
-    if (point < min && max < candidate) {
-        return point;
+
+    if (left <= min && min <= right && right <= max) {
+        return flag == 0 ? min : right;
     }
-   
-    if (point > max && min > candidate && flag == 1) {
-        return point;
+
+    if (min <= left && max >= left && max <= right) {
+        return flag == 0 ? left : max;
     }
-    return flag == 1 ? max : min;
+
+    if (left <= min && max <= right) {
+        return flag == 0 ? min: max;
+    }
+
+    return 0;
 }
 
 int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+    int lapArea = 0;
 
-    int left = inArea(E, A, C, G, 0);
-    int bottom = inArea(B, F, H, D, 0);
-    int right = inArea(C, E, G, A, 1);
-    int top = inArea(H, B, D, F, 1);
+    int left = inArea(E, G, A, C, 0);
+    int bottom = inArea(B, D, F, H, 0);
+    int right = inArea(A, C, E, G, 1);
+    int top = inArea(F, H, B, D, 1);
 
     cout<<left<<" "<<bottom<<" "<<right<<" "<<top<<endl;
-
     int width = abs(right - left);
     int height = abs(top- bottom);
 
-    return width * height;
+    lapArea =  width * height;
+
+    int one_area = abs(C - A) * abs(D - B);
+    int ano_area = abs(G - E) * abs(H - F);  
+
+    cout<<"lapArea: "<<lapArea<<" first area:"<<one_area<<" another area:"<<ano_area<<endl;
+    if (!one_area) {
+        return ano_area;  
+    }
+
+    if (!ano_area) {
+        return one_area;
+    }
+
+    if (one_area == lapArea ) {
+        return ano_area > lapArea ? ano_area  : lapArea;
+    }
+
+    if (ano_area == lapArea) {
+        return one_area > lapArea ? one_area : lapArea;
+    }
+
+    // plus two area and minus the lap area
+    return   one_area + ano_area - lapArea;
 }
 
 int main(int argc, char *argv[]) {
-    cout<<computeArea(-3, 0, 3, 4, 0, -1, 9, 2)<<endl;
+    cout<<computeArea(0, 0, 0, 0, -1, -1, 1, 1)<<endl;
     return 0;
 }
 
